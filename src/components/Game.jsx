c25682362d0b73ed40b16ce2d46f180feedf5451
Jie Xing <jiexing@Jies-MacBook-Pro.local>
2018-03-11 10:50:46 -0400

    Follow tutorial

---
+++ src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Board from "./Board";
+
+export default class Game extends Component {
+
+    render() {
+        return (
+            <div className={"game"}>
+            <div className={"game-board"}>
+                <Board/>
+            </div>
+        </div>
+        );
+    }
+}
