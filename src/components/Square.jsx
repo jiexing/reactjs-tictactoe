2aee7c7c9fb3fd0bb22c0e75a0b1aa8d192cfc34
DESKTOP-11LGPP8\jiexi <jie.xing@gmail.com>
2018-03-11 11:34:35 -0400

    State in Game component.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 export default class Square extends React.Component {
+
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
